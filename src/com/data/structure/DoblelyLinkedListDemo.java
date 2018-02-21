6fe683832f3285ff82564edcf7243ebc6d394442
Vijay Kumar <vijay.kumar@kohls.com>
2018-02-21 19:09:44 +0530

    added forward and backout direction to show the element with add on position

--- src/com/data/structure/DoblelyLinkedListDemo.java
+++ src/com/data/structure/DoblelyLinkedListDemo.java
@@ -9,50 +9,89 @@ public class DoblelyLinkedListDemo {
         list.add(true);
         list.add(60.30);
         list.add('@');
-		list.add("six");
-		list.show();
+		//list.addFirst("First");
+		//list.addFirst("FirstMost");
+		//list.addLast("Last");
+		//list.addLast("LastMost");
+		System.out.println("Forward Direction>>>");
+		list.showForward();
+		System.out.println("Forward Direction>>> before adding position ");
+		list.addPosition(3, "Mid");
+		System.out.println(" after adding");
+		System.out.println("Forward Direction>>>");
+		list.showForward();
+		System.out.println("\nBackward Direction<<<");
 		list.showBackward();
 		System.out.println("\nEndend");
     }
 }
 
 class DoubleLinkedList{
-    Node head,tail,temp;
+    Node head,tail,temp,temp2;
     
-    public void show(){
+    public void showForward(){
         temp = head;
-        while (temp!= tail) {
+        while (temp!= null) {
             System.out.print(temp.data + "\t");
 			temp = temp.next;
 		}
-		System.out.print(temp.data + "\t");
     }
-	
 	
     public void showBackward(){
         temp = tail;
-        while (temp!= head) {
+        while (temp!= null) {
             System.out.print(temp.data + "\t");
 			temp = temp.previous;
 		}
-	//	System.out.print(temp.data + "\t");
     }
 	
 	public void add(Object o){
         if(head ==  null){
            head = tail = new Node(o);
+		   head.previous = null;
         }else{
-            temp = head;
-            while (temp != tail){
-                temp = temp.next;
-            }
-            temp.next = new Node(o);
-			temp.previous = temp;
-			tail = temp.next;
+			temp=new Node(o);
+			tail.next=temp;
+			temp.previous=tail;
+			tail = temp;
         }
     }
-   
-
+	public void addFirst(Object o){
+		temp = new Node(o);
+		temp.next = head;
+		head.previous = temp;
+		head = temp;
+	}
+	
+	public void addLast(Object o){
+		temp = new Node(o);
+		tail.next = temp;
+		temp.previous = tail;
+		tail = temp;
+	}
+	
+	public void addPosition(int num, Object o){
+		Node n = new Node(o);
+		temp = head;
+		for(int i=1; i < num-1; i++){
+			temp = temp.next;
+		}
+		
+		
+		 /* n.next = temp.next;
+		temp.next=n;
+		n.previous = temp; 
+		temp.next.previous=n;  */
+		  temp2 = temp.next;
+		temp.next=n;
+		n.previous=temp;
+		n.next=temp2;
+		temp2.previous=n; 
+		
+		
+	}
+	
+	
 }
 
 class Node{
